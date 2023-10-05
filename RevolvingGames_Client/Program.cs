0e8a3477bc460508cf63cee9b5d4255ba4d5b89f
ali-kiyani <malikiyani4@gmail.com>
2023-10-06 01:27:31 +0500

    client base

---
+++ RevolvingGames_Client/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using RevolvingGamesServer;
+
+Console.WriteLine("Client running");
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7118");
+var client = new RevolvingService.RevolvingServiceClient(channel);
+var res = await client.CheckPrimeNmberAsync(new PrimeNumber()
+{
+    Id = 2,
+    Number = 1,
+    Timestamp = 1
+});
+Console.WriteLine(res.ToString());
