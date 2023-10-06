b45f62735be323b90735caed142fdc77451e4607
ali-kiyani <malikiyani4@gmail.com>
2023-10-07 02:40:00 +0500

    added gRPC streams and sending requests and receiving responses asynchronously

--- RevolvingGames_Client/Program.cs
+++ RevolvingGames_Client/Program.cs
@@ -1,15 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Grpc.Net.Client;
-using RevolvingGamesServer;
+﻿using RevolvingGames_Client;
 
-Console.WriteLine("Client running");
-
-using var channel = GrpcChannel.ForAddress("https://localhost:7118");
-var client = new RevolvingService.RevolvingServiceClient(channel);
-var res = await client.CheckPrimeNmberAsync(new PrimeNumber()
-{
-    Id = 2,
-    Number = 1,
-    Timestamp = 1
-});
-Console.WriteLine(res.ToString());
+Client client= new Client();
+await client.ClientExeAsync();
